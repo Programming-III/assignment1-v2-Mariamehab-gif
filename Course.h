3a56e39ca0ff1908153e8c8cf793e8dbbc9764d5
Mariamehab-gif <mariamelhosseiny.com@gmail.com>
2025-11-11 10:03:18 +0200

    Refactor Course.h header guards and includes
    Fixed header guards and included correct header files.

--- Course.h
+++ Course.h
@@ -1,9 +1,25 @@
-#ifndef COURSE_H
-#define COURSE_H
 
+#ifndef assignment1.h
+#define assignment1.h
+#include <iostream>
 #include <string>
-#include "Student.h"
+#include "assignemnt1.h"
+#include "assignemnt1.h"
 using namespace std;
+class Course{
+private:
+	string CourseCode;
+	string CourseName;
+	int maxStudents;
+	Student* students;
+	int currentStudents;
+
+public:
+	void addStudent(const Student& s);
+	addStudent(const Student& s);
+	void displayCourseInfo();
+};
+#endif
 //#write your code here
 
 
