8244e70ce1da1081efbc9427d4f00da5d3c1d36c
Mariamehab-gif <mariamelhosseiny.com@gmail.com>
2025-11-11 10:06:56 +0200

    Refactor class definitions and add display methods
    Fixed class definitions and added display methods for Person, Student, Instructor, and Course classes.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,49 @@
+#include "assignemnt1.h"
+#include "assignmentt.h"
 #include <iostream>
 #include <string>
-#include "Person.h"
-#include "Student.h"
-#include "Instructor.h"
-#include "Course.h"
-
-
 using namespace std;
+void Person::class Person(string Name, int Id;) {
+	this->Name = Name;
+	this->Id = Id;
+}
+void Student::class Student(int YearLevel, string Major) {
+	this->YearLevel = YearLevel;
+	this->Major = Major;
+}
+void Instructor:: class Instructor(string Department, int ExperienceYears) {
+	this->Departement = Department;
+	this->ExperienceYears = ExperienceYears;
+}
+void Course:: class Course(string CourseCode, string CourseName, int maxStudents, Student* students, int CurrentStudents) {
+	this->CourseCode = CourseCode;
+	this->CourseName = CourseName;
+	this->maxStudents = maxStudents;
+	this->CurrentStudents = CurrentStudents;
+}
+	void Person::display() {
+		cout << "name:" << Name;
+		cout << "id:" << Id;
+
+	}
+	void Student::displayInfo() {
+		cout << "YearLevel:" << YearLevel << endl;
+		cout << "major:" << Major << endl;
+	}
+	void instructor::display() {
+		cout << "department:" << Department << endl;
+		cout << "experienceYear:" << ExperienceYear << endl;
+
+	}
+	class Course::addStudent(const Student& s){
+	}
+	void Course::displayCourseInfo() {
+		cout << "CourseCode:" << Course::CourseCode << endl;
+		cout << "CourseName" << Course::CourseName << endl;
+		cout << "maxStudents" << Course::maxStudents << endl;
+	 }
+	;
+
 
 // ==================== Person Class Implementation =========================
 
