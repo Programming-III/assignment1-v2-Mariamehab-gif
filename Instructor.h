12e1bdb96213a66b73994325726924011a25da86
Mariamehab-gif <mariamelhosseiny.com@gmail.com>
2025-11-11 10:05:52 +0200

    Create Instructor.h with Person, Student, and instructor classes
    Defined Person, Student, and instructor classes with display methods.

--- Instructor.h
+++ Instructor.h
@@ -1,19 +1,27 @@
-#ifndef INSTRUCTOR_H
-#define INSTRUCTOR_H
-
-#include "Person.h"
+#ifndef assignment1.h
+#define assignment1.h
+#include <iostream>
+#include <string>
 using namespace std;
-//#write Instructor class here
-
-
-
-
-
-
-
-
-
-
-
-
+class Person {
+private:
+	string Name;
+	int Id;
+public:
+	void display();
+};
+class Student {
+private:
+	int YearLevel;
+	string Major;
+public:
+	void displayInfo();
+};
+class instructor {
+private:
+	string Department;
+	int ExperienceYear;
+public:
+	void display();
+};
 #endif
